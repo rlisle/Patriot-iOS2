1926be1348325ebe59c4d63366b0363780de5269
Ron Lisle <ron.lisle@mutualmobile.com>
2018-05-27 07:27:23 -0500

    Add slide transition

--- Patriot/View Controllers/TabBarController.swift
+++ Patriot/View Controllers/TabBarController.swift
@@ -17,6 +17,7 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        delegate = self
         selectLoginTab()
     }
     
@@ -32,4 +33,84 @@ class TabBarController: UITabBarController {
         selectedIndex = 2
     }
     
+//    func animateToTab(toIndex: Int) {
+//        let tabViewControllers = viewControllers!
+//        let fromView = selectedViewController!.view
+//        let toView = tabViewControllers[toIndex].view
+//        let fromIndex = tabViewControllers.indexOf(selectedViewController!)
+//
+//        guard fromIndex != toIndex else {return}
+//
+//        // Add the toView to the tab bar view
+//        fromView.superview!.addSubview(toView)
+//
+//        // Position toView off screen (to the left/right of fromView)
+//        let screenWidth = UIScreen.mainScreen().bounds.size.width;
+//        let scrollRight = toIndex > fromIndex;
+//        let offset = (scrollRight ? screenWidth : -screenWidth)
+//        toView.center = CGPoint(x: fromView.center.x + offset, y: toView.center.y)
+//
+//        // Disable interaction during animation
+//        view.userInteractionEnabled = false
+//
+//        UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+//
+//            // Slide the views by -offset
+//            fromView.center = CGPoint(x: fromView.center.x - offset, y: fromView.center.y);
+//            toView.center   = CGPoint(x: toView.center.x - offset, y: toView.center.y);
+//
+//        }, completion: { finished in
+//
+//            // Remove the old view from the tabbar view.
+//            fromView.removeFromSuperview()
+//            self.selectedIndex = toIndex
+//            self.view.userInteractionEnabled = true
+//        })
+//    }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        print("tabBarController shouldSelect called")
+        guard let fromView = selectedViewController?.view, let toView = viewController.view else {
+            return false
+        }
+        let tabViewControllers = viewControllers!
+        let fromIndex = tabViewControllers.index(of: selectedViewController!)!   // YOLO
+        let toIndex = tabViewControllers.index(of: viewController)!
+        guard fromIndex != toIndex else {return false}
+
+        // Add the toView to the tab bar view
+        fromView.superview!.addSubview(toView)
+        
+        // Position toView off screen (to the left/right of fromView)
+        let screenWidth = UIScreen.main.bounds.size.width;
+        let scrollRight = toIndex > fromIndex;
+        let offset = (scrollRight ? screenWidth : -screenWidth)
+        toView.center = CGPoint(x: fromView.center.x + offset, y: toView.center.y)
+        
+        // Disable interaction during animation
+        view.isUserInteractionEnabled = false
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            
+            // Slide the views by -offset
+            fromView.center = CGPoint(x: fromView.center.x - offset, y: fromView.center.y);
+            toView.center   = CGPoint(x: toView.center.x - offset, y: toView.center.y);
+            
+        }, completion: { finished in
+            
+            // Remove the old view from the tabbar view.
+            fromView.removeFromSuperview()
+            self.selectedIndex = toIndex
+            self.view.isUserInteractionEnabled = true
+        })
+
+        
+        
+        return true
+    }
+
 }
